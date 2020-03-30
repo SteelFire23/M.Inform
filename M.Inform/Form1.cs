b81d3ab6078dcd16e1e1c992343040fcf90907c4
SteelFire23 <parturnaks23@gmail.com>
2020-03-30 12:24:10 +0300

    Добавьте файлы проекта.

---
+++ M.Inform/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace M.Inform
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 form = new Form2();
+            form.Owner = this;
+            form.Show();
+            Close();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 1; i < 6; i++)
+            {
+                comboBox1.Items.Add(i);
+            }
+            for (int i = 1; i < 6; i++)
+            {
+                comboBox2.Items.Add(i);
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                checkBox2.Checked = false;
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked)
+            {
+                checkBox1.Checked = false;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ M.Inform/Form2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace M.Inform
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            Form1 main = Owner as Form1;
+            string text = "";
+            if (main.checkBox1.Checked)
+            {
+                text = "бакалавриата";
+            }
+            else if (main.checkBox2.Checked)
+            {
+                text = "магистратуры";
+            }
+            textBox1.Text = "Справка с места учёбы\n";
+            textBox1.AppendText($"{Environment.NewLine}Студент {text} {main.textBox1.Text}, обучающийся на {main.comboBox1.Text} курсе специальности {main.textBox2.Text} в группе {main.comboBox2.Text}.");
+            textBox1.AppendText($" Действительно обучается в высшем учебном заведении.");
+            textBox1.AppendText($"{Environment.NewLine}Подпись _____                Дата: {DateTime.Now.ToShortDateString()}");
+        }
+    }
+}
